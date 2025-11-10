4b6210e0bcc241f4d933b5e8085353e75a79fcdd
ffybcesin <ffybcesin@gmail.com>
2025-11-10 08:31:31 -0300

    se agrego admin y se probo subir los 4 articulos falta modificar el listar

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { ItemListContainer } from './components/ItemListContainer/ItemListContai
 import { ItemDetailContainer } from './components/ItemDetailContainer/ItemDetailcontainer';
 import { CartProvider } from './context/Cartcontext/CartProvider';
 import { Cart } from "./components/Cart/Cart";
+import { ProductFormContainer } from './components/adminComponents/ProductFormContainer/ProductFormContainer';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path='/detail/:id' element={<ItemDetailContainer />} /> 
           <Route path='/category/:category' element={<ItemListContainer titulo='Categoría' />} /> 
           <Route path='/cart' element={<Cart />} /> 
+          <Route path='/admin' element={<ProductFormContainer />} /> 
         </Routes>
       </CartProvider>
     </BrowserRouter>
