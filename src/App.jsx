4d3e7034bc32f6f50bb9af375f7d01dc1b57c773
ffybcesin <ffybcesin@gmail.com>
2025-11-12 12:03:18 -0300

    se corrigio app ItemDetailcontainer

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import { Footer } from "./components/Footer/Footer";
 /*import { Header } from "./components/Header/Header";*/
-import { ItemDetailContainer } from "./components/ItemDetailContainer/ItemDetailContainer";
+import { ItemDetailContainer } from "./components/ItemDetailContainer/ItemDetailcontainer";
+//import { ItemDetailContainer } from "./components/ItemDetailContainer/ItemDetailContainer";
 import { ItemListContainer } from "./components/ItemListContainer/ItemListContainer";
 import { CartProvider } from "./context/CartContext/CartProvider";
 import { Cart } from "./components/Cart/Cart";
