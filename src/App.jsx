9aa64ecea1f67a15d0a7280f87d115c145eb1fd9
ffybcesin <ffybcesin@gmail.com>
2025-11-12 11:45:52 -0300

    se corrigio title en index

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import { Footer } from "./components/Footer/Footer";
-import { Header } from "./components/Header/Header";
+/*import { Header } from "./components/Header/Header";*/
 import { ItemDetailContainer } from "./components/ItemDetailContainer/ItemDetailContainer";
 import { ItemListContainer } from "./components/ItemListContainer/ItemListContainer";
 import { CartProvider } from "./context/CartContext/CartProvider";
